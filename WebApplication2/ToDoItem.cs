0164b3b1b1154b58995306071e5f15f253b0db49
tim@hexandale.com <tim@hexandale.com>
2019-08-12 16:05:46 +0100

    Initial commit

---
+++ WebApplication2/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
